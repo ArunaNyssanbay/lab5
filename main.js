bdf69de4bcfe7aded64ffff6dc059bc14cae74cf
ArunaNyssanbay <122426797+ArunaNyssanbay@users.noreply.github.com>
2023-02-28 22:21:39 +0600

    Initial commit

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-let column_1=[1,4,7,8,4,9,2,3,8,5,6,3,2,7,6,5,6,7,5,4,2,1,5,9,3,2,7,8,9,3];
-let column_2=[9,6,2,3,4,7,8,9,1,2,3,4,5,6,7,9,7,5,4,3,2,3,4,5,6,1,2,8,5,4];
-let column_3=[41,42,66,77,84,54,66,90,72,43,52,83,56,78,89,90,99,43,54,65,76,87,81,64,67,89,67,77,54,51];
-let column_4=[42,52,62,62,72,82,92,43,53,63,73,83,93,44,85,45,46,47,66,48,49,50,51,52,53,54,55,56,57,75];
-let sum_1=column_1[0]+column_1[1]+column_1[2]+column_1[3]+column_1[4]+column_1[5]+column_1[6]+column_1[7]+column_1[8]+column_1[9]+column_1[10]+column_1[11]+column_1[12]+column_1[13]+column_1[14]+column_1[15]+column_1[16]+column_1[17]+column_1[18]+column_1[19]+column_1[20]+column_1[21]+column_1[22]+column_1[23]+column_1[24]+column_1[25]+column_1[26]+column_1[27]+column_1[28]+column_1[29];
-let sum_2=column_2[0]+column_2[1]+column_2[2]+column_2[3]+column_2[4]+column_2[5]+column_2[6]+column_2[7]+column_2[8]+column_2[9]+column_2[10]+column_2[11]+column_2[12]+column_2[13]+column_2[14]+column_2[15]+column_2[16]+column_2[17]+column_2[18]+column_2[19]+column_2[20]+column_2[21]+column_2[22]+column_2[23]+column_2[24]+column_2[25]+column_2[26]+column_2[27]+column_2[28]+column_2[29];
-let sum_3=column_3[0]+column_3[1]+column_3[2]+column_3[3]+column_3[4]+column_3[5]+column_3[6]+column_3[7]+column_3[8]+column_3[9]+column_3[10]+column_3[11]+column_3[12]+column_3[13]+column_3[14]+column_3[15]+column_3[16]+column_3[17]+column_3[18]+column_3[19]+column_3[20]+column_3[21]+column_3[22]+column_3[23]+column_3[24]+column_3[25]+column_3[26]+column_3[27]+column_3[28]+column_3[29];
-let sum_4=column_4[0]+column_4[1]+column_4[2]+column_4[3]+column_4[4]+column_4[5]+column_4[6]+column_4[7]+column_4[8]+column_4[9]+column_4[10]+column_4[11]+column_4[12]+column_4[13]+column_4[14]+column_4[15]+column_4[16]+column_4[17]+column_4[18]+column_4[19]+column_4[20]+column_4[21]+column_4[22]+column_4[23]+column_4[24]+column_4[25]+column_4[26]+column_4[27]+column_4[28]+column_4[29];
-let sums=[sum_1,sum_2,sum_3,sum_4];
-let final_result=sums[0]+sums[1]+sums[2]+sums[3];
-
+let a=5;
+let b=10;
+//assignment 1//
+console.log("TASK_1")
+let perimeter=2*a+2*b;
 const results={
-	col1: column_1,
-	col2: column_2,
-	col3: column_3,
-	col4: column_4,
-	sums: sums,
-	total_score: final_result 
+	A_side: a,
+	B_side: b,
+	perimeter_of_the_rectangle: perimeter,
 }
 console.log(results)
+//assignment 2//
+console.log("TASK_2")
+let x=60
+let y=25
+console.log("Number x is "+x)
+console.log("Number y is "+y)
+if (x<y){
+	console.log("Number y is the greatest one")
+}
+else if(x>y){
+	console.log("Number x is the greatest one")
+}
+//assignment 3//
+console.log("TASK_3")
+let KZT=500000
+console.log("Today 1 US dollar equls to 445 kazakhstani tenge. Let's convert 500 000 tenge to US dollars:")
+let USD=0.0022*KZT
+console.log("500 000/0.0022 => "+KZT+" tenge equals to "+USD+" US dollars")
+
+
+
+ 
